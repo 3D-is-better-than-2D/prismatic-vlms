e22adcaf5c9f580f1afe619cc3cb54f9103bc096
JohnYao93 <yaojohn6@gmail.com>
2025-05-13 09:55:35 +0000

    basic unfiltered dataset

---
+++ dataset.py
@@ -0,0 +1,15 @@
+from datasets import load_dataset
+
+# Login using e.g. `huggingface-cli login` to access this dataset
+ds = load_dataset("remyxai/OpenSpaces_MC")
+
+# Print the dataset
+print(ds)
+# Print the dataset splits
+print(ds.keys())
+# Print a specific question/answer pair for image 0
+print(ds['train']['messages'][0][2])
+# Show there is image and messages
+print(ds['train'])
+# Print number of Q/A pairs for image 0
+print(len(ds['train']['messages'][0]))
